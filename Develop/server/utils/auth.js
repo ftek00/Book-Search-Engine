6e0e82497d9a1d2764e3738818f4b014f4b85600
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-11-11 19:48:56 -0500

    updated auth.js to be compatible with apollo server

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,36 +1,41 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { GraphQLError } = require("graphql");
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
 module.exports = {
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
+    // Error for unauthenticated users
+    extensions: {
+      code: "UNAUTHENTICATED",
+    },
+  }),
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    if (token && token.startsWith("Bearer ")) {
+      token = token.split(" ").pop().trim(); // Extract token part
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return { user: null };
     }
 
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return { user: data };
+    } catch (error) {
+      console.log("Invalid token:", error);
+      return { user: null };
     }
-
-    // send to next endpoint
-    next();
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
